32b6656ea25352eb4b12425241b87df6659cdea5
oleksandrblazhko <blazhko@ieee.org>
2026-08-14 22:15:59 +0300

    commit

--- textworld/core.py
+++ textworld/core.py
@@ -254,6 +254,10 @@ class Environment:
 
         outfile.write(msg + "\n")
 
+        print("DEBUG feedback:", repr(self.state.feedback), file=sys.stderr)
+        print("DEBUG stdout encoding:", sys.stdout.encoding, file=sys.stderr)
+        print("DEBUG stdout errors:", sys.stdout.errors, file=sys.stderr)
+
         if mode == "text":
             outfile.seek(0)
             return outfile.read()
